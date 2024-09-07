3ba2a4027d49f9992c145dd19a517da45069e157
Lee Yoon Seok <kjggr123@gmail.com>
2024-09-08 02:07:03 +0900

    Initial commit

---
+++ Source/Wild/Game/Character/WildMainCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Wild/WildCharacter.h"
+#include "WildMainCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WILD_API AWildMainCharacter : public AWildCharacter
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/Wild/WildGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "WildGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AWildGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AWildGameMode();
+};
+
+
+
