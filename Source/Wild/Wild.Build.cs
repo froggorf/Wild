fd90f946503a640c3625e596550220144faee563
Lee Yoon Seok <kjggr123@gmail.com>
2024-09-09 03:32:32 +0900

    무기 컴퍼넌트 제작 및 애니메이션 몽타쥬 재생 확인

--- Source/Wild/Wild.Build.cs
+++ Source/Wild/Wild.Build.cs
@@ -8,6 +8,6 @@ public class Wild : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "Chooser" });
 	}
 }
